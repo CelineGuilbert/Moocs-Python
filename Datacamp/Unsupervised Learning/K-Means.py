853def86c697861655608345f94df85807dfd989
CelineGuilbert <guilbert.cel@gmail.com>
2017-02-22 08:51:53 +0100

    Update K-Means.py

--- Datacamp/Unsupervised Learning/K-Means.py
+++ Datacamp/Unsupervised Learning/K-Means.py
@@ -1,7 +1,51 @@
+# # Import KMeans
+from sklearn.cluster import KMeans
+
+# Create a KMeans instance with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Fit model to points
+model.fit(points)
+
+# Determine the cluster labels of new_points: labels
+labels = model.predict(new_points)
+
+# Print cluster labels of new_points
+print(labels)
+
+
+
+# Import pyplot
+import matplotlib.pyplot as plt
+
+# Assign the columns of new_points: xs and ys
+xs = new_points[:,0]
+ys = new_points[:,1]
+# Make a scatter plot of xs and ys, using labels to define the colors
+plt.scatter(xs,ys,c=labels,alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = model.cluster_centers_
+
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+
+# Make a scatter plot of centroids_x and centroids_y
+plt.scatter(centroids_x,centroids_y, marker='D', s=50)
+plt.show()
+
+
+
+
+
+# Avec une Standardisation
+
 # Perform the necessary imports
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+import pandas as pd
 
 # Create scaler: scaler
 scaler = StandardScaler()
@@ -11,3 +55,39 @@ kmeans = KMeans(n_clusters=4)
 
 # Create pipeline: pipeline
 pipeline = make_pipeline(scaler,kmeans)
+
+# Fit the pipeline to samples
+pipeline.fit(samples)
+
+# Calculate the cluster labels: labels
+labels = pipeline.predict(samples)
+
+# Create a DataFrame with labels and species as columns: df
+df = pd.DataFrame({'labels': labels, 'species': species})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'],df['species'])
+
+# Display ct
+print(ct)
+
+
+
+
+# Avec une Normalisation
+
+# Import Normalizer
+from sklearn.preprocessing import Normalizer
+
+# Create a normalizer: normalizer
+normalizer = Normalizer()
+
+# Create a KMeans model with 10 clusters: kmeans
+kmeans = KMeans(n_clusters=10)
+
+# Make a pipeline chaining normalizer and kmeans: pipeline
+pipeline = make_pipeline(normalizer,kmeans)
+
+# Fit pipeline to the daily price movements
+pipeline.fit(movements)
+
