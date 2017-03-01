e3d694ff661749dfcd78d552dc1ab9cd78d60e7b
CelineGuilbert <guilbert.cel@gmail.com>
2017-03-01 08:07:15 +0100

    Update ECDF.py

--- Datacamp/Statistical Thinking in Python/ECDF.py
+++ Datacamp/Statistical Thinking in Python/ECDF.py
@@ -15,7 +15,7 @@ def ecdf(data):
     x = np.sort(data) #Return a sorted copy of an array.
 
     # y-data for the ECDF: y
-    y = np.arange( 1, n+1) / n #the end value in np.arange() is not inclusive # le résultat final ira de 0.2 à 1
+    y = np.arange( 1, n+1) / n #the end value in np.arange() is not inclusive 
 
     return x, y
   
@@ -61,3 +61,18 @@ _ = plt.ylabel('ECDF')
 
 # Display the plot
 plt.show()
+
+
+##
+##print(x_set)  #50 valeurs
+#[ 1.   1.1  1.2  1.2  1.3  1.3  1.3  1.3  1.3  1.3  1.3  1.4  1.4  1.4  1.4
+#  1.4  1.4  1.4  1.4  1.4  1.4  1.4  1.4  1.5  1.5  1.5  1.5  1.5  1.5  1.5
+# 1.5  1.5  1.5  1.5  1.5  1.5  1.5  1.6  1.6  1.6  1.6  1.6  1.6  1.6  1.7
+# 1.7  1.7  1.7  1.9  1.9]
+
+##y_set : divise toutes les valeurs par 50
+#[ 0.02  0.04  0.06  0.08  0.1   0.12  0.14  0.16  0.18  0.2   0.22  0.24
+#  0.26  0.28  0.3   0.32  0.34  0.36  0.38  0.4   0.42  0.44  0.46  0.48
+#  0.5   0.52  0.54  0.56  0.58  0.6   0.62  0.64  0.66  0.68  0.7   0.72
+# 0.74  0.76  0.78  0.8   0.82  0.84  0.86  0.88  0.9   0.92  0.94  0.96
+#  0.98  1.  ]
