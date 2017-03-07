a98bd32d4f459a35479ba96cc3a27047285f9e45
CelineGuilbert <guilbert.cel@gmail.com>
2017-03-07 12:52:54 +0100

    Update functions.py

--- Dataquest/functions.py
+++ Dataquest/functions.py
@@ -87,3 +87,38 @@ def tokenize(text_string, special_characters):
 
 tokenized_story = tokenize(story_string, clean_chars)
 print(tokenized_story[0:10])
+
+
+
+
+#Finding Misspelled Words
+#Create an empty list named misspelled_words.
+#Write a for loop that:
+#Iterates over tokenized_story.
+#Uses an if statement that evaluates whether the current token is in tokenized_vocabulary and if it isn't, appends the current token to misspelled_words.
+#Print misspelled_words.
+
+def clean_text(text_string, special_characters):
+    cleaned_string = text_string
+    for string in special_characters:
+        cleaned_string = cleaned_string.replace(string, "")
+    cleaned_string = cleaned_string.lower()
+    return(cleaned_string)
+
+def tokenize(text_string, special_characters):
+    cleaned_story = clean_text(text_string, special_characters)
+    story_tokens = cleaned_story.split(" ")
+    return(story_tokens)
+
+misspelled_words = []
+clean_chars = [",", ".", "'", ";", "\n"]
+tokenized_story = tokenize(story_string, clean_chars)
+tokenized_vocabulary = tokenize(vocabulary, clean_chars)
+for ts in tokenized_story:
+    if ts not in tokenized_vocabulary:
+        misspelled_words.append(ts)
+print(misspelled_words)
+print(misspelled_words)
+
+
+
