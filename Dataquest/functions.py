ad1c0376f2c3dc38cdc5535fa9fcb2ddabaec822
CelineGuilbert <guilbert.cel@gmail.com>
2017-03-07 12:31:22 +0100

    Update functions.py

--- Dataquest/functions.py
+++ Dataquest/functions.py
@@ -34,3 +34,30 @@ def clean_text(text_string):
 cleaned_story = clean_text(story_string)
 
 
+#Modify the clean_text() function below in the following ways:
+#Add another argument called special_characters.
+#Modify the function body so that it loops over all special_characters that are punctuation marks, and removes each one:
+#Assign the input variable text_string to cleaned_string.
+#Write a for loop that:
+#Iterates over the input variable special_characters.
+#Uses replace() to remove all of the instances of the current special character in cleaned_string.
+#Outside the loop, use lower() to make all the text in cleaned_string lowercase, and then return it.
+#Once you've written the function, run clean_text():
+#Pass in story_string as the first argument.
+#Pass in clean_chars as the second argument.
+#Assign the result to cleaned_story.
+#Print cleaned_story.
+
+f = open("story.txt", 'r')
+story_string = f.read()
+clean_chars = [",", ".", "'", ";", "\n"]
+
+def clean_text(text_string, special_characters):
+    cleaned_string = text_string
+    for string in special_characters:
+        cleaned_string = cleaned_string.replace(string, "")
+    cleaned_string = cleaned_string.lower()
+    return(cleaned_string)
+
+cleaned_story = clean_text(story_string, clean_chars)
+print(cleaned_story)
