2db3f2004ecd6ec530710516ae433033896fee5d
CelineGuilbert <guilbert.cel@gmail.com>
2017-03-10 08:01:34 +0100

    Update opening_files.py

--- Dataquest/opening_files.py
+++ Dataquest/opening_files.py
@@ -1,29 +1,57 @@
+# 1 - Opening Files
+
+#To open a file in Python, we use the open() function. 
+#This function accepts two different arguments (inputs) in the parentheses, always in the following order:
+    #the name of the file (as a string)
+    #the mode of working with the file (as a string) --- "r" = the mode for reading in files.
+
+#The open() function returns a File object
 open("story.txt", "r")
 text = open("story.txt", "r")
 
+
+# 2 - Readind Files
+
+#File objects have a read() method that returns a string representation of the text in a file. 
+#Unlike the append() method from the previous mission, the read() method returns a value instead of modifying the object 
+#that calls the method.
+
+    #Note that the File object, text, won't contain the actual contents of the file. 
+    #It's instead an object that acts as an interface to the file
+    #and contains methods for reading in and modifying the file's contents 
+
 #<_io.TextIOWrapper name='test.txt' mode='r' encoding='UTF-8'>
-#"r" = the mode for reading in files.
-
-#Note that the File object, text, won't contain the actual contents of the file. 
-#It's instead an object that acts as an interface to the file
-#and contains methods for reading in and modifying the file's contents 
-
-#File objects have a read() method that returns a string representation of the text in a file
-
 f = open("test.txt", "r")
 g = f.read()
 
 
-#fonction split piur convertir en liste
+# 3 - To make our string object data more useful, let's convert it into a list. Here's a preview of how the dataset looks:
+#Each line is separated by the string \n, which is referred to as the new-line character
+    #Albuquerque,749\nAnaheim,371\nAnchorage,828\n
+#the split() method to turn a string object into a list of strings
 
 f = open('crime_rates.csv', 'r')
 data = f.read()
 rows = data.split("\n")
 print(rows)
 
-# List of Lists
-#1. splits each element in three_rows (which contains the first three elements from rows) on the comma delimiter (,)
-#2. appends the resulting list (split_list) to a new list we create (final_list)
+
+# 4 - Loops
+#We now have a list representation of the dataset (rows). 
+#Each element in the dataset is a string containing a comma (,) that separates the city name from the crime rate. 
+#Because they're strings, we can use the split() method on each of them to separate those values. 
+
+
+ten_rows = rows[0:10]
+
+for item in ten_rows:
+    print(item)
+    
+    
+# 5 - List of Lists
+
+#1. splits each element in three_rows (which contains the first three elements from rows) on the comma delimiter (,) -> String
+#2. appends the resulting list (split_list) to a new list we create (final_list)  -> List
 #3. displays the final list using the print() function
 
 three_rows = ["Albuquerque,749", "Anaheim,371", "Anchorage,828"]
@@ -33,9 +61,11 @@ for row in three_rows:
     split_list = row.split(',')
     final_list.append(split_list)
 print(final_list)
+print(final_list[0])
 
 
-####
+
+# 6 - Splitting Elements In A List
 f = open('crime_rates.csv', 'r')
 data = f.read()
 rows = data.split('\n')
@@ -43,15 +73,26 @@ print(rows[0:5])
 
 final_data = []
 for row in rows:
+     # row is a list variable, not a string / Text is a string
     text  = row.split(',')
+    #final_data is a list
     final_data.append(text)
 print(final_data)
         
         
         
-#Accessing elements in a list after retreating
+# 7 : Accessing Elements In A List Of Lists
 
-print(five_elements)
+#A list of lists has some unique interaction mechanisms. 
+#Using bracket notation to retrieve an element at a certain index returns a list object. 
+#However, using bracket notation on the resulting list will actually return a data point (such as a string or an integer).
+
+print(five_elements) #List of List
+            [['Albuquerque', '749'],
+             ['Anaheim', '371'],
+             ['Anchorage', '828'],
+             ['Arlington', '503'],
+             ['Atlanta', '1379']]
 cities_list = []
 cities_list.append(five_elements[0][0])
 cities_list.append(five_elements[1][0])
@@ -60,7 +101,7 @@ cities_list.append(five_elements[3][0])
 cities_list.append(five_elements[4][0])
 
 
-#With a loop
+    #With a loop
 
 crime_rates = []
 cities_list = []
@@ -69,7 +110,7 @@ for row in five_elements:
     crime_rate = row[1]
     # crime_rate is a string, the crime rate of the city.
     crime_rates.append(crime_rate)
-    
+            #print crime_rates = ['749', '371', '828', '503', '1379']
 for row in final_data:
     citie_list = row[0]
     cities_list.append(citie_list)
