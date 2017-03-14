7f707835d23b84b52771ccc0bb2ef2f680f0c161
Céline Guilbert <guilbert.cel@gmail.com>
2017-03-14 23:07:33 +0100

    Create getting_starting_with_numpy.py

---
+++ Dataquest/getting_starting_with_numpy.py
@@ -0,0 +1,16 @@
+import csv
+f = open("world_alcohol.csv")
+reader = csv.reader(f)
+world_alcohol = list(reader)
+
+years = []
+for row in world_alcohol:
+    years.append(row[0])
+
+years = years[1:]
+
+total = 0
+for year in years:
+    total = total + float(year)
+
+avg_year = total / len(years)
