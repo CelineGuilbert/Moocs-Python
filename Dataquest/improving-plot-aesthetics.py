3e153076d2ac9655bdaaf555b957d64df58280c6
Céline Guilbert <guilbert.cel@gmail.com>
2017-03-15 13:12:09 +0100

    Update improving-plot-aesthetics.py

--- Dataquest/improving-plot-aesthetics.py
+++ Dataquest/improving-plot-aesthetics.py
@@ -47,10 +47,11 @@ plt.show()
 
 
 major_cats = ['Biology', 'Computer Science', 'Engineering', 'Math and Statistics']
-fig = plt.figure(figsize=(12, 12))
+fig = plt.figure(figsize=(12, 12)) # figsize=(width,height) of each graphic
 
-for sp in range(0,4):
-    ax = fig.add_subplot(2,2,sp+1)
+for sp in range(0,4): #4 pck il y a 4 modalités dans major_cats donc on demande 4 graphiques
+    ax = fig.add_subplot(2,2,sp+1)  #= subplot layout, ici deux graphs par ligne donc 2 lignes / 2 colonnes
+                                    #  pour avoir les graph sur une seule ligne (1,4,sp+1)
     ax.plot(women_degrees['Year'], women_degrees[major_cats[sp]], c='blue', label='Women')
     ax.plot(women_degrees['Year'], 100-women_degrees[major_cats[sp]], c='green', label='Men')
     # Add your code here.
@@ -65,8 +66,8 @@ plt.show()
 major_cats = ['Biology', 'Computer Science', 'Engineering', 'Math and Statistics']
 fig = plt.figure(figsize=(12, 12))
 
-for sp in range(0,4):
-    ax = fig.add_subplot(2,2,sp+1)
+for sp in range(0,4): 
+    ax = fig.add_subplot(2,2,sp+1) #the subplot layout 
     ax.plot(women_degrees['Year'], women_degrees[major_cats[sp]], c='blue', label='Women')
     ax.plot(women_degrees['Year'], 100-women_degrees[major_cats[sp]], c='green', label='Men')
     for key,spine in ax.spines.items():
@@ -78,4 +79,41 @@ for sp in range(0,4):
     
 # Calling pyplot.legend() here will add the legend to the last subplot that was created.
 plt.legend(loc='upper right')
+plt.show()
+
+
+# Setting Line Color Using RGB
+cb_orange = (255/255,128/255,14/255)
+ ax.plot(women_degrees['Year'], 100-women_degrees[major_cats[sp]], c=cb_orange, label='Men')
+# Setting Line Width
+ ax.plot(women_degrees['Year'], 100-women_degrees[major_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+  
+  
+ #Annotating In Matplotlib
+
+  #Add the following text annotations in the leftmost chart:
+    #The string "Men" at the x-axis coordinate of 2005 and the y-axis coordinate of 87.
+    #The string "Women" at the x-axis coordinate of 2002 and the y-axis coordinate of 8.
+  #Add the following text annotations in the rightmost chart:
+    #The string "Men" at the x-axis coordinate of 2005 and the y-axis coordinate of 62.
+    #The string "Women" at the x-axis coordinate of 2001 and the y-axis coordinate of 35.
+
+
+fig = plt.figure(figsize=(18, 3))
+for sp in range(0,6):
+    ax = fig.add_subplot(1,6,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[stem_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[stem_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    for key,spine in ax.spines.items():
+        spine.set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(stem_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+    if sp == 0:
+            ax.text(2005,87,"Men")
+            ax.text(2002,8,"Women")
+    if sp == 5:
+            ax.text(2005,62,"Men")
+            ax.text(2001,35,"Women")
 plt.show()
