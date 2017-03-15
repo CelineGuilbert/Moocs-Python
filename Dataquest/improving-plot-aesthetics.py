fc82870bda385de11f957d6bd97a4b5f528a4133
Céline Guilbert <guilbert.cel@gmail.com>
2017-03-15 12:45:29 +0100

    Update improving-plot-aesthetics.py

--- Dataquest/improving-plot-aesthetics.py
+++ Dataquest/improving-plot-aesthetics.py
@@ -58,3 +58,24 @@ for sp in range(0,4):
 # Calling pyplot.legend() here will add the legend to the last subplot that was created.
 plt.legend(loc='upper right')
 plt.show()
+
+
+
+#improve precedent example by adding title of each graph and an unique x/y limit axis
+major_cats = ['Biology', 'Computer Science', 'Engineering', 'Math and Statistics']
+fig = plt.figure(figsize=(12, 12))
+
+for sp in range(0,4):
+    ax = fig.add_subplot(2,2,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[major_cats[sp]], c='blue', label='Women')
+    ax.plot(women_degrees['Year'], 100-women_degrees[major_cats[sp]], c='green', label='Men')
+    for key,spine in ax.spines.items():
+        spine.set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(major_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+    
+# Calling pyplot.legend() here will add the legend to the last subplot that was created.
+plt.legend(loc='upper right')
+plt.show()
