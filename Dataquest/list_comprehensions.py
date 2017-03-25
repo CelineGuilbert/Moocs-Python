c318f481495add23b17721715e15dc8a9a49a1b5
Céline Guilbert <guilbert.cel@gmail.com>
2017-03-25 20:13:12 +0100

    Update list_comprehensions.py

--- Dataquest/list_comprehensions.py
+++ Dataquest/list_comprehensions.py
@@ -127,3 +127,23 @@ top_female_names = []
 for key in name_counts:#name_counts is a dict
     if name_counts[key] == 2:
         top_female_names.append(key)
+
+ ##Most commom Name for male
+top_male_names = []
+male_name_counts = {}
+for row in legislators:
+    if row[3] == "M" and row[7] > 1940:
+        name = row[1]
+        if name in male_name_counts:
+            male_name_counts[name] = male_name_counts[name] + 1
+        else:
+            male_name_counts[name] = 1
+
+highest_value = None
+for name,count in male_name_counts.items():
+    if highest_value is None or count > highest_value:
+        highest_value = count
+
+for name,count in male_name_counts.items():
+    if count == highest_value:
+        top_male_names.append(name)
