6dad44546b847da29d58c9ce8b33ff8a26695f5f
Céline Guilbert <guilbert.cel@gmail.com>
2017-03-25 22:36:12 +0100

    Update challenge-modules-classes-error-handling-and-list-comprehensions.py

--- Dataquest/challenge-modules-classes-error-handling-and-list-comprehensions.py
+++ Dataquest/challenge-modules-classes-error-handling-and-list-comprehensions.py
@@ -1,4 +1,7 @@
-import csv
+#CSV : https://github.com/fivethirtyeight/data/blob/master/nfl-suspensions/nfl-suspensions-data.csv
+    
+    
+    import csv
 f = open("nfl_suspensions_data.csv", 'r')
 csvreader = csv.reader(f)
 nfl_suspensions = list(csvreader)
@@ -20,3 +23,32 @@ for row in nfl_suspensions:
 unique_teams=set(unique_teams)
 
 unique_games=set([row[2] for row in nfl_suspensions])
+
+
+
+class Suspension():
+    def __init__(self,row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2] 
+        self.year = row[5]
+third_suspension = Suspension(nfl_suspensions[2])
+
+
+
+
+class Suspension():
+    def __init__(self,row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2] 
+        try:
+            self.year = int(row[5])
+        except Exception:
+             self.year = 0
+    def get_year(self):
+        return(self.year)
+                
+missing_year = Suspension(nfl_suspensions[22])
+twenty_third_year = missing_year.get_year() ##23e_year is a int
+
