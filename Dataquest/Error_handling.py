a0af07ecc2b3845b365eb5a9de06fe83f624341e
Céline Guilbert <guilbert.cel@gmail.com>
2017-03-25 18:06:31 +0100

    Update Error_handling.py

--- Dataquest/Error_handling.py
+++ Dataquest/Error_handling.py
@@ -34,5 +34,30 @@ print(gender)
 
 birth_years=[]
 for item in legislators:
-    parts= item[2].split("-")
+    parts= item[2].split("-") #parts est une liste /// item[2] est un str
     birth_years.append(parts[0])
+    
+    
+##6: Try/Except Blocks
+#int('') : The code above will cause a ValueError, because an empty string can't be converted to an integer.
+try:
+    int('')
+except Exception:  #Exception is a Type
+    print("There was an error")
+#In the example above, the Python interpreter will try to run int(''), and generate a ValueError.
+#Instead of stopping the code from executing, it will be handled by the except statement, which will print the message 
+#There was an error.
+#The Python interpreter will continue to run any lines of code that come after the except statement.
+    
+    
+    
+##7: Exception Instances
+#When the Python interpreter generates an exception, it actually creates an instance of the Exception class.
+#This class has certain properties that help us debug the error. 
+#We can access the instance of the Exception class in the except statement body:
+try:
+    int('')
+except Exception as exc:
+    print(type(exc)) ##<class 'ValueError'>
+    print(str(exc)) ##invalid literal for int() with base 10: ''
+#In the example above, we use the as statement to assign the instance of the Exception class to the variable exc
