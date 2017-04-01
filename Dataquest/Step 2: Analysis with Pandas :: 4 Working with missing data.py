1f6f6001816590aa27e2f8d5c0f5b8206caff98e
Céline Guilbert <guilbert.cel@gmail.com>
2017-04-01 23:12:03 +0200

    Update Step 2: Analysis with Pandas :: 4 Working with missing data.py

--- Dataquest/Step 2: Analysis with Pandas :: 4 Working with missing data.py
+++ Dataquest/Step 2: Analysis with Pandas :: 4 Working with missing data.py
@@ -27,3 +27,38 @@ correct_mean_age = sum(good_ages) / len(good_ages)
 #For example, if we use use the Series.mean() method to calculate the mean of a column, missing values will not be included in the
 #calculation.
 correct_mean_fare= titanic_survival["fare"].mean()
+
+
+##5: Calculating Summary Statistics
+#Use a for loop to iterate over passenger_classes. Within the for loop:
+  #Select just the rows in titanic_survival where the pclass value is equivalent to the current iterator value (class).
+  #Select just the fare column for the current subset of rows.
+  #Use the Series.mean method to calculate the mean of this subset.
+  #Add the mean of the class to the fares_by_class dictionary with class as the key.
+#Once the loop completes, the dictionary fares_by_class should have 1, 2, and 3 as keys, with the average fares as the corresponding 
+#values.
+
+passenger_classes = [1, 2, 3]
+fares_by_class = {}
+
+for classe in passenger_classes:
+    pclass_rows = titanic_survival[titanic_survival['pclass']== classe]
+    pclass_fares= pclass_rows['fare']
+    fare_for_class = pclass_fares.mean()
+    fares_by_class[classe]=fare_for_class
+            
+      
+##6: Making Pivot Tables
+#Luckily, the Dataframe.pivot_table() method instead, which simplifies the kind of work we did on the last screen. 
+#To produce the same data, we could use one line.
+
+passenger_survival = titanic_survival.pivot_table(index="pclass", values="fare", aggfunc=np.mean)
+#calcule la valeur moyenne de "fare" par "pclass"
+#The default for the aggfunc parameter is actually the mean, so if we're calculating this we can omit this parameter.
+
+
+## 7: More Complex Pivot Tables
+#pass a list of column names to the values parameter instead of a single value
+import numpy as np
+port_stats = titanic_survival.pivot_table(index="embarked", values=["fare","survived"], aggfunc=np.sum)
+print(port_stats)
