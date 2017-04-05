595fa62d5550f81c77994f73e76e77b746fe1522
Céline Guilbert <guilbert.cel@gmail.com>
2017-04-05 22:13:13 +0200

    Create Step 2 : 6 :: challenge-summarizing-data.py

---
+++ Dataquest/Step 2 : 6 :: challenge-summarizing-data.py
@@ -0,0 +1,63 @@
+##Summarizing Major Categories
+
+# Unique values in Major_category column.
+print(all_ages['Major_category'].unique())
+
+aa_cat_counts = dict()
+rg_cat_counts = dict()
+def calculate_major_cat_totals(df):
+    cats = df['Major_category'].unique()
+    counts_dictionary = dict()
+
+    for c in cats:
+        major_df = df[df["Major_category"] == c]
+        total = major_df["Total"].sum()
+        counts_dictionary[c] = total
+    return counts_dictionary
+
+aa_cat_counts = calculate_major_cat_totals(all_ages)
+rg_cat_counts = calculate_major_cat_totals(recent_grads)
+
+
+
+##Low-Wage Job Rates
+
+
+low_wage_percent = 0.0
+low_wage_jobs_sum = recent_grads['Low_wage_jobs'].sum()
+recent_grads_sum = recent_grads['Total'].sum()
+low_wage_percent = low_wage_jobs_sum / recent_grads_sum
+print(low_wage_percent)
+
+
+
+
+##5: Comparing Data Sets
+majors = recent_grads['Major'].unique()
+rg_lower_count = 0
+for m in majors:
+    recent_grads_row = recent_grads[recent_grads['Major'] == m]
+    all_ages_row = all_ages[all_ages['Major'] == m]
+    
+    rg_unemp_rate = recent_grads_row.iloc[0]['Unemployment_rate']
+    aa_unemp_rate = all_ages_row.iloc[0]['Unemployment_rate']
+    
+    if rg_unemp_rate < aa_unemp_rate:
+        rg_lower_count += 1
+
+print(rg_lower_count)
+
+
+ou mon script:
+
+majors = recent_grads['Major'].unique()
+rg_lower_count = 0
+
+
+for major in majors:
+    all_ages_row = all_ages[all_ages['Major']== major]
+    recent_grads_row = recent_grads[recent_grads["Major"]== major]
+    if float(recent_grads_row['Unemployment_rate']) < float(all_ages_row['Unemployment_rate']):
+        rg_lower_count +=1
+
+print(rg_lower_count)
