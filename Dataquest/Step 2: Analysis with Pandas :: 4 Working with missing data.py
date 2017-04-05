60f68f1c08b8be5b682c75d61a4738b8c8ac3f61
Céline Guilbert <guilbert.cel@gmail.com>
2017-04-05 20:01:15 +0200

    Update Step 2: Analysis with Pandas :: 4 Working with missing data.py

--- Dataquest/Step 2: Analysis with Pandas :: 4 Working with missing data.py
+++ Dataquest/Step 2: Analysis with Pandas :: 4 Working with missing data.py
@@ -62,3 +62,99 @@ passenger_survival = titanic_survival.pivot_table(index="pclass", values="fare",
 import numpy as np
 port_stats = titanic_survival.pivot_table(index="embarked", values=["fare","survived"], aggfunc=np.sum)
 print(port_stats)
+
+##                fare  survived
+##embarked                      
+##C         16830.7922     150.0
+##Q          1526.3085      44.0
+##S         25033.3862     304.0
+
+##8: Drop Missing Values
+#The dropna() method takes an axis parameter, which indicates whether you would like to drop rows or columns. 
+#Specifying axis=0 or axis='index' will drop any rows that have null values, 
+##while specifying axis=1 or axis='columns' will drop #any columns that have null values.
+#We will use 0 and 1 since they're more commonly used, but you can use either.
+
+drop_na_rows = titanic_survival.dropna(axis=0)
+#Drop all rows in titanic_survival where the columns "age" or "sex" have missing and values assign the result to new_titanic_survival.
+new_titanic_survival = titanic_survival.dropna(axis=0,subset=["age", "sex"])
+
+
+##9: Using Iloc To Access Rows By Position
+#Assign the first ten rows from new_titanic_survival to first_ten_rows.
+first_ten_rows = new_titanic_survival.iloc[0:10]
+#Assign the fifth row from new_titanic_survival to row_position_fifth.
+row_position_fifth = new_titanic_survival.iloc[4]
+#Assign the row with index label 25 from new_titanic_survivalto row_index_25.
+row_index_25 = new_titanic_survival.loc[25]
+
+#Assign the value at row index label 1100, column index label "age" from new_titanic_survival to row_index_1100_age.
+row_index_1100_age=new_titanic_survival.loc[1100,"age"]
+#Assign the value at row index label 25, column index label "survived" from new_titanic_survival to row_index_25_survived.
+row_index_25_survived=new_titanic_survival.loc[25,"survived"]
+#Assign the first 5 rows and first three columns from new_titanic_survival to five_rows_three_cols.
+five_rows_three_cols=new_titanic_survival.iloc[0:5,0:3]  
+
+
+##11: Reindexing Rows
+#Sometimes it's useful to reindex, starting from 0. We can use the DataFrame.reset_index() method to do this.
+#By default, the method retains the old index by adding an extra column to the dataframe with the old index values.
+
+#http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.reset_index.html
+
+#Reindex the new_titanic_survival dataframe so the row indexes start from 0, and the old index is dropped.
+titanic_reindexed=new_titanic_survival.reset_index(drop=True)
+print(titanic_reindexed.iloc[0:5,0:3])
+
+
+##12: Apply Functions Over A DataFrame
+#To perform a complex calculation across pandas objects, we'll need to learn about the DataFrame.apply() method. 
+#By default, DataFrame.apply() will iterate through each column in a DataFrame, and perform on each function
+
+# This function returns the hundredth item from a series
+def hundredth_row(column):
+    # Extract the hundredth item
+    hundredth_item = column.iloc[99]
+    return hundredth_item
+
+# Return the hundredth item from each column
+hundredth_row = titanic_survival.apply(hundredth_row)
+
+#Write a function that counts the number of null elements in a Series.
+import pandas as pd 
+def isnull(column):
+    column_null = pd.isnull(column)
+    null = column[column_null]
+    return len(null)
+
+column_null_count = titanic_survival.apply(isnull)
+
+
+##13: Applying A Function To A Row
+def which_class(row):
+    pclass = row['pclass']
+    if pd.isnull(pclass):
+        return "Unknown"
+    elif pclass == 1:
+        return "First Class"
+    elif pclass == 2:
+        return "Second Class"
+    else pclass == 3:
+        return "Third Class"
+
+classes = titanic_survivors.apply(which_class, axis=1)
+
+
+
+import pandas as pd
+
+def generate_age_label(row):
+    age = row["age"]
+    if pd.isnull(age):
+        return "unknown"
+    elif age < 18:
+        return "minor"
+    else:
+        return "adult"
+
+age_labels = titanic_survival.apply(generate_age_label, axis=1)
