f528ff0f54fc7226fa01f8235401b8dace4f37c7
Céline Guilbert <guilbert.cel@gmail.com>
2018-01-23 11:04:50 +0000

    Update PySpark.py

--- Datacamp/PySpark.py
+++ Datacamp/PySpark.py
@@ -1,3 +1,7 @@
+
+#Sparksession created = spark
+
+
 # Print the tables in the catalog
 print(spark.catalog.listTables())
 
@@ -13,3 +17,36 @@ flights10 = spark.sql(query)
 
 # Show the results
 flights10.show()
+
+
+# Don't change this query
+query = "SELECT origin, dest, COUNT(*) as N FROM flights GROUP BY origin, dest"
+
+# Run the query
+flight_counts = spark.sql(query)
+
+# Convert the results to a pandas DataFrame
+pd_counts = flight_counts.toPandas()
+
+# Print the head of pd_counts
+print(pd_counts.head())
+
+
+
+
+# Create pd_temp
+pd_temp = pd.DataFrame(np.random.random(10))
+
+# Create spark_temp from pd_temp
+spark_temp = spark.createDataFrame(pd_temp)
+
+# Examine the tables in the catalog
+print(spark.catalog.listTables())
+
+# Add spark_temp to the catalog
+spark_temp.createOrReplaceTempView("temp")
+
+# Examine the tables in the catalog again
+print(spark.catalog.listTables())
+
+
