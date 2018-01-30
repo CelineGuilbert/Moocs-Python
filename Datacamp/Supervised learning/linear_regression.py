07bdadcfe27239e34fe3b33984614a8db12abaf3
Céline Guilbert <guilbert.cel@gmail.com>
2018-01-30 07:10:32 +0000

    Update linear_regression.py

--- Datacamp/Supervised learning/linear_regression.py
+++ Datacamp/Supervised learning/linear_regression.py
@@ -90,3 +90,18 @@ print(np.mean(cvscores_10))
 #0.87 --> 11.5 ms
 #0.84 --> 37.3 ms
 
+
+
+## Regularization Lasso
+
+# Import Lasso
+from sklearn.linear_model import Lasso
+# Instantiate a lasso regressor: lasso
+lasso = Lasso(alpha=0.4, normalize=True)
+# Fit the regressor to the data
+lasso.fit(X, y)
+# Compute and print the coefficients
+lasso_coef = lasso.coef_print(lasso_coef)
+# Plot the coefficients
+plt.plot(range(len(df_columns)), lasso_coef)plt.xticks(range(len(df_columns)), df_columns.values, rotation=60)plt.margins(0.02)plt.show()
+
