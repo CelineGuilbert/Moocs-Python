869b94e3666eb2936bd974dca5567cd80bf20629
Céline Guilbert <guilbert.cel@gmail.com>
2018-01-31 21:56:17 +0100

    Update Fine-tuning your model.py

--- Datacamp/Supervised learning/Fine-tuning your model.py
+++ Datacamp/Supervised learning/Fine-tuning your model.py
@@ -86,3 +86,41 @@ print("Best score is {}".format(tree_cv.best_score_))
  #   Best score is 0.7291666666666666
 
 #Note that RandomizedSearchCV will never outperform GridSearchCV. Instead, it is valuable because it saves on computation time
+
+
+## Hold-out set
+
+# Import necessary modules
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+# Create the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {"C": c_space, "penalty": ['l1', 'l2']}
+
+# Instantiate the logistic regression classifier: logreg
+logreg = LogisticRegression()
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.4, random_state=42)
+
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv=5 )
+
+# Fit it to the training data
+logreg_cv.fit(X_train, y_train)
+
+# Print the optimal parameters and best score
+print("Tuned Logistic Regression Parameter: {}".format(logreg_cv.best_params_))
+print("Tuned Logistic Regression Accuracy: {}".format(logreg_cv.best_score_))
+
+
+#Remember lasso and ridge regression from the previous chapter? 
+#Lasso used the L1 penalty to regularize, while ridge used the L2 penalty
+# There is another type of regularized regression known as the elastic net. 
+##  a∗L1+b∗L2
+
+
+
+
