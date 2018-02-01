6dedeace0e2d78a674973f22db826a124a53ad3e
Céline Guilbert <guilbert.cel@gmail.com>
2018-02-01 21:15:01 +0100

    Update preprocessing_data.py

--- Datacamp/Supervised learning/preprocessing_data.py
+++ Datacamp/Supervised learning/preprocessing_data.py
@@ -26,3 +26,15 @@ print(df_region.columns)
 
 
 
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+
+# Instantiate a ridge regressor: ridge
+ridge = Ridge(alpha=0.5, normalize=True)
+
+# Perform 5-fold cross-validation: ridge_cv
+ridge_cv = cross_val_score(ridge, X,y , cv=5)
+
+# Print the cross-validated scores
+print(ridge_cv)
